e3fe5e5353f3cdc5e622ff27b8e6fcea3792f311
Praveen Gupta <pvgupta24@gmail.com>
2018-04-15 21:13:35 +0530

    Table Lookup Delay added

--- getTLD.m
+++ getTLD.m
@@ -1,6 +1,10 @@
 % Function to find the Table Lookup Delay(TLD)
 
 function TLD = getTLD(a)
-    TLD = exprnd(1/a)
+    %TLD = 0.01*exprnd(1/a);
+    a = 0.053;
+    b = 0.06;
+    TLD = (b-a)*rand() + a;
+    
 end
 
